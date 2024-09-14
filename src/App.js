4eeab7c5bdb66f6ac7a61ccd9c3074d2e56e58a0
lkw <zgbtlkw@gmail.com>
2024-09-14 08:49:25 +0000

    add content

--- src/App.js
+++ src/App.js
@@ -1,52 +1,98 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+
 import QuizData from './QuizData';
 
 const App = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answerUp, setAnswerUp] = useState(1);
   const [score, setScore] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [userAnswer, setUserAnswer] = useState('');
   const [feedback, setFeedback] = useState('');
   const [showCorrectAnswer, setShowCorrectAnswer] = useState(false); // 用于显示正确答案
 
+  const logRandomBinary = () => {
+    const newAnswerUp = Math.round(Math.random());
+    setAnswerUp(newAnswerUp);
+    if (newAnswerUp != answerUp){
+      console.log('swap', newAnswerUp);
+    
+    }
+    console.log('answerUp changed to:', newAnswerUp);
+  };
+  
+  
   const handleAnswerInput = (event) => {
     const input = event.target.value;
+    console.log('change input:');
+    if (input.includes('=') || input.includes('【') || input.includes('】')) {
+      return; // 如果输入包含 '='，直接返回，不更新 userAnswer
+    }
     setUserAnswer(input);
-
-    if (input.toLowerCase() === QuizData[currentQuestion].correctAnswer.toLowerCase()) {
+    if (answerUp === 0) {
+      if (input.toLowerCase() === QuizData.sentences[currentQuestion].questionText.toLowerCase()) {
+        setFeedback('Correct!');
+        setScore(score + 1);
+      } else {
+        setFeedback(''); // 当答案不正确时清除反馈
+      }
+      return 
+    }
+    if (input.toLowerCase() === QuizData.sentences[currentQuestion].correctAnswer.toLowerCase()) {
       setFeedback('Correct!');
       setScore(score + 1);
-
-      setTimeout(() => {
-        setFeedback('');
-        setUserAnswer('');
-        setShowCorrectAnswer(false); // 清除显示的正确答案
-        const nextQuestion = currentQuestion + 1;
-        if (nextQuestion < QuizData.length) {
-          setCurrentQuestion(nextQuestion);
-        } else {
-          setShowScore(true);
-        }
-      }, 1000); // 1秒后自动跳转到下一个问题
     } else {
-      setFeedback(''); // 当答案不正确时，清除反馈
+      setFeedback(''); 
     }
   };
 
+  const qDiv = <div className='question-section'>
+    <div className='question-text'>{QuizData.sentences[currentQuestion].questionText}</div>
+  </div>
+  const qDiv2 = <div className='question-section'>
+    <div className='question-text'>{QuizData.sentences[currentQuestion].correctAnswer}</div>
+  </div>
+
+const aDiv = <div className='answer-section'>
+    <input
+      type='text'
+      value={userAnswer}
+      onChange={handleAnswerInput}
+      placeholder='Enter your answer'
+    />
+
+  </div>
   // 添加键盘事件监听
   useEffect(() => {
+    // console.log('current change:' + currentQuestion);
     const handleKeyDown = (event) => {
       if (event.key === '=') {
+        event.preventDefault(); // 阻止默认行为
         setShowCorrectAnswer(true); // 当按下 '=' 键时显示正确答案
       }
       if (event.key === ']') {
-        setCurrentQuestion(currentQuestion + 1); // 当按下 '=' 键时显示正确答案
+        logRandomBinary()
+        setFeedback('');
+        setUserAnswer('');
+        setShowCorrectAnswer(false);
+        setCurrentQuestion(currentQuestion == QuizData.sentences.length - 1 ? 0 : currentQuestion + 1);
+      }
+      if (event.key === '[') {
+        logRandomBinary()
+
+        setFeedback('');
+        setUserAnswer('');
+        setShowCorrectAnswer(false);
+        setCurrentQuestion(currentQuestion === 0 ? QuizData.sentences.length - 1 : currentQuestion - 1); 
       }
     };
 
     window.addEventListener('keydown', handleKeyDown);
 
+    
+    
+    
     // 清除事件监听
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
@@ -55,31 +101,39 @@ const App = () => {
 
   return (
     <div className='app'>
+      <h3>
+      {QuizData.title}
+      </h3>
       {showScore ? (
         <div className='score-section'>
-          You scored {score} out of {QuizData.length}
+          You scored {score} out of {QuizData.sentences.length}
         </div>
       ) : (
         <>
-          <div className='question-section'>
-            <div className='question-count'>
-              <span>Question {currentQuestion + 1}</span>/{QuizData.length}
-            </div>
-            <div className='question-text'>{QuizData[currentQuestion].questionText}</div>
+          <div className='question-count'>
+            <span>Question {currentQuestion + 1}</span>/{QuizData.sentences.length}
           </div>
-          <div className='answer-section'>
-            <input
-              type='text'
-              value={userAnswer}
-              onChange={handleAnswerInput}
-              placeholder='Enter your answer'
-            />
+          <div className='question-answer-container'>
+
+          {answerUp === 1 ? (
+            <>
+              {qDiv}
+              {aDiv}
+            </>
+          ) : (
+            <>
+              {aDiv}
+              {qDiv2}
+            </>
+          )}
+
             <div className='feedback'>{feedback}</div>
             {showCorrectAnswer && (
               <div className='correct-answer'>
-                Correct Answer: {QuizData[currentQuestion].correctAnswer}
+                Correct Answer: {QuizData.sentences[currentQuestion].questionText},  {QuizData.sentences[currentQuestion].correctAnswer}
               </div>
             )}
+
           </div>
         </>
       )}
--- src/QuizData.js
+++ src/QuizData.js
@@ -1,20 +1,296 @@
-const QuizData = [
+const QuizData2 = [
   {
-    questionText: 'What is the capital of France?',
-    correctAnswer: 'Paris',
+    questionText: '闻道长安似弈棋',
+    correctAnswer: '百年世事不胜悲',
   },
   {
-    questionText: 'Who is CEO of Tesla?',
-    correctAnswer: 'Elon Musk',
+    questionText: '王侯第宅皆新主',
+    correctAnswer: '文武衣冠异昔时',
   },
   {
-    questionText: 'The iPhone was created by which company?',
-    correctAnswer: 'Apple',
+    questionText: '直北关山金鼓振',
+    correctAnswer: '征西车马羽书驰',
   },
   {
-    questionText: 'How many Harry Potter books are there?',
-    correctAnswer: '7',
+    questionText: '鱼龙寂寞秋江冷',
+    correctAnswer: '故国平居有所思',
+  },]
+
+  const a =  [{
+    questionText: '将军魏武之子孙',
+    correctAnswer: '于今为庶为清门',
   },
-];
+  {
+    questionText: '英雄割据虽已矣',
+    correctAnswer: '文采风流今尚存',
+  },
+  {
+    questionText: '学书初学卫夫人',
+    correctAnswer: '但恨无过王右军',
+  },
+  {
+    questionText: '丹青不知老将至',
+    correctAnswer: '富贵于我如浮云',
+  },
+  {
+    questionText: '开元之中常引见',
+    correctAnswer: '承恩数上南薰殿',
+  },
+  {
+    questionText: '凌烟功臣少颜色',
+    correctAnswer: '将军下笔开生面',
+  },
+  {
+    questionText: '良相头上进贤冠',
+    correctAnswer: '猛将腰间大羽箭',
+  },
+  {
+    questionText: '褒公鄂公毛发动',
+    correctAnswer: '英姿飒爽来酣战',
+  },
+  {
+    questionText: '先帝御马玉花骢',
+    correctAnswer: '画工如山貌不同',
+  },
+  {
+    questionText: '是日牵来赤墀下',
+    correctAnswer: '迥立阊阖生长风',
+  },
+  
+  {
+    questionText: '诏谓将军拂绢素',
+    correctAnswer: '意匠惨淡经营中',
+  },
+  {
+    questionText: '斯须九重真龙出',
+    correctAnswer: '一洗万古凡马空',
+  },
+  // 至尊含笑催赐金，圉人太仆皆惆怅
+  // 弟子韩幹早入室，亦能画马穷殊相
+  // 幹惟画肉不画骨，忍使骅骝气凋丧？
+  {
+    //   玉花却在御榻上，榻上庭前屹相向
+    questionText: '玉花却在御榻上',
+    correctAnswer: '榻上庭前屹相向',
+  },
+  {
+    questionText: '天下何曾有山水',
+    correctAnswer: '人间不解重骅骝',
+  },
+  {
+    questionText: '斯须九重真龙出',
+    correctAnswer: '一洗万古凡马空',
+  },
+  {
+    questionText: '渭北春天树',
+    correctAnswer: '江东日暮云',
+  },
+  {
+    questionText: '白也诗无敌',
+    correctAnswer: '飘然思不群',
+  },
+  ];
+
+const PoemData = [
+  {
+    questionText: '千家山郭静朝晖',
+    correctAnswer: '日日江楼坐翠微',
+  },
+  {
+    questionText: '信宿渔人还泛泛',
+    correctAnswer: '清秋燕子故飞飞',
+  },
+  {
+    questionText: '匡衡抗疏功名薄',
+    correctAnswer: '刘向传经心事违',
+  },
+  {
+    questionText: '同学少年多不贱',
+    correctAnswer: '五陵衣马自轻肥',
+  }
+]
+const PoemData4 = [
+  {
+    //   夔府孤城落日斜，每依北斗望京华
+    questionText: '夔府孤城落日斜',
+    correctAnswer: '每依北斗望京华',
+  },
+  // 听猿实下三声泪，奉使虚随八月槎
+  // 画省香炉违伏枕，山楼粉堞隐悲笳
+  // 请看石上藤萝月，已映洲前芦荻花
+  {
+    questionText: '听猿实下三声泪',
+    correctAnswer: '奉使虚随八月槎',
+  },
+  {
+    questionText: '匡衡抗疏功名薄',
+    correctAnswer: '刘向传经心事违',
+  },
+  {
+    questionText: '同学少年多不贱',
+    correctAnswer: '五陵衣马自轻肥',
+  }
+]
+
+const PoemData2 = [
+{
+  questionText: '少陵野老吞声哭',
+  correctAnswer: '春日潜行曲江曲',
+},
+{
+  questionText: '江头宫殿锁千门',
+  correctAnswer: '细柳新蒲为谁绿',
+},
+// 
+// 忆昔霓旌下南苑
+// 苑中万物生颜色
+{
+  questionText: '忆昔霓旌下南苑',
+  correctAnswer: '苑中万物生颜色',
+},
+// 昭阳殿里第一人
+// 同辇随君侍君侧
+{
+  questionText: '昭阳殿里第一人',
+  correctAnswer: '同辇随君侍君侧',
+},
+// 辇前才人带弓箭
+// 白马嚼啮黄金勒
+{
+  questionText: '辇前才人带弓箭',
+  correctAnswer: '白马嚼啮黄金勒',
+},
+// 翻身向天仰射云
+// 一笑正坠双飞翼
+{
+  questionText: '翻身向天仰射云',
+  correctAnswer: '一笑正坠双飞翼',
+},
+// 明眸皓齿今何在
+// 血污游魂归不得
+{
+  questionText: '明眸皓齿今何在',
+  correctAnswer: '血污游魂归不得',
+},
+// 清渭东流剑阁深
+// 去住彼此无消息
+{
+  questionText: '清渭东流剑阁深',
+  correctAnswer: '去住彼此无消息',
+},
+// 人生有情泪沾臆
+// 江草江花岂终极
+{
+  questionText: '人生有情泪沾臆',
+  correctAnswer: '江草江花岂终极',
+},
+  {
+    questionText: '欲往城南望城北',
+    correctAnswer: '黄昏胡骑尘满城',
+  },
+]
+const PoemData3 = [
+  //   车辚辚，马萧萧 行人弓箭各在腰
+  {
+    questionText: '车辚辚，马萧萧',
+    correctAnswer: '行人弓箭各在腰',
+  },
+  // 爷娘妻子走相送，尘埃不见咸阳桥
+  {
+    questionText: '爷娘妻子走相送',
+    correctAnswer: '尘埃不见咸阳桥',
+  },
+  // 牵衣顿足拦道哭，哭声直上干云霄
+  {
+    questionText: '牵衣顿足拦道哭',
+    correctAnswer: '哭声直上干云霄',
+  },
+  // 道旁过者问行人，行人但云点行频
+  {
+    questionText: '道旁过者问行人',
+    correctAnswer: '行人但云点行频',
+  },
+  // 或从十五北防河，便至四十西营田
+  {
+    questionText: '或从十五北防河',
+    correctAnswer: '便至四十西营田',
+  },
+  // 去时里正与裹头，归来头白还戍边
+  {
+    questionText: '去时里正与裹头',
+    correctAnswer: '归来头白还戍边',
+  },
+  // 边庭流血成海水，武皇开边意未已
+  {
+    questionText: '边庭流血成海水',
+    correctAnswer: '武皇开边意未已',
+  },
+  // 君不闻：汉家山东二百州，千村万落生荆杞
+  {
+    questionText: '君不闻汉家山东二百州',
+    correctAnswer: '千村万落生荆杞',
+  },
+  // 纵有健妇把锄犁，禾生陇亩无东西
+  {
+    questionText: '纵有健妇把锄犁',
+    correctAnswer: '禾生陇亩无东西',
+  },
+  // 况复秦兵耐苦战，被驱不异犬与鸡
+  {
+    questionText: '况复秦兵耐苦战',
+    correctAnswer: '被驱不异犬与鸡',
+  },
+  
+  // 长者虽有问，役夫敢申恨
+  {
+    questionText: '长者虽有问',
+    correctAnswer: '役夫敢申恨',
+  },
+  // 且如今年冬，未休关西卒
+  {
+    questionText: '且如今年冬',
+    correctAnswer: '未休关西卒',
+  },
+  // 县官急索租，租税从何出
+  {
+    questionText: '县官急索租',
+    correctAnswer: '租税从何出',
+  },
+  // 信知生男恶，反是生女好
+  {
+    questionText: '信知生男恶',
+    correctAnswer: '反是生女好',
+  },
+  // 生女犹得嫁比邻 生男埋没随百草
+  {
+    questionText: '生女犹得嫁比邻',
+    correctAnswer: '生男埋没随百草',
+  },
+  // 君不见青海头 古来白骨无人收
+  {
+    questionText: '君不见青海头',
+    correctAnswer: '古来白骨无人收',
+  },
+  // 新鬼烦冤旧鬼哭 天阴雨湿声啾啾！ 
+  {
+    questionText: '新鬼烦冤旧鬼哭',
+    correctAnswer: '天阴雨湿声啾啾',
+  },
+]
+
+
+const QuizData = {
+  // title : '哀江头',
+  title : '兵车行',
+  // title : '秋兴八首三',
+  // title : '秋兴八首四',
+  sentences : PoemData3
+}
+
+
+
+
 
 export default QuizData;
+
+
