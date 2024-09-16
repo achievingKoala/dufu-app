a9ff02a8957b547d74a5198b6e83ab36e633463c
lkw <zgbtlkw@gmail.com>
2024-09-16 18:48:02 +0000

    show collect

--- src/App.js
+++ src/App.js
@@ -36,16 +36,11 @@ const App = () => {
         titleMap[sentence.title] = {
           title: sentence.title,
           key: `collected-${sentence.title}`,
-          children: []
+          children: [],
+          fromCollect: true
         };
         collected.children.push(titleMap[sentence.title]);
       }
-
-      titleMap[sentence.title].children.push({
-        title: sentence.content,
-        key: `collected-sentence-${index}`,
-        isLeaf: true
-      });
     });
 
     return [collected];
@@ -98,41 +93,41 @@ const App = () => {
     }
   };
 
-  const qDiv = 
-  <div className='question-section'>
-    <div className='question-text'>{quizData[currentQuestion].questionText}</div>
-  </div>
-  
-  const qDiv2 = 
-  <div className='question-section'>
-    <div className='question-text'>{quizData[currentQuestion].correctAnswer}</div>
-  </div>
+  const qDiv =
+    <div className='question-section'>
+      <div className='question-text'>{quizData[currentQuestion].questionText}</div>
+    </div>
 
-  const aDiv = 
-  <div className='answer-section'>
-    <input
-      type='text'
-      value={userAnswer}
-      onChange={handleAnswerInput}
-      placeholder='Enter your answer'
-    />
-  </div>
+  const qDiv2 =
+    <div className='question-section'>
+      <div className='question-text'>{quizData[currentQuestion].correctAnswer}</div>
+    </div>
 
-  const questionSpan = 
+  const aDiv =
+    <div className='answer-section'>
+      <input
+        type='text'
+        value={userAnswer}
+        onChange={handleAnswerInput}
+        placeholder='Enter your answer'
+      />
+    </div>
+
+  const questionSpan =
     <span className='question-text'> {quizData[currentQuestion].questionText}</span>
-  
-    const questionSpan2 = 
+
+  const questionSpan2 =
     <span className='question-text'> {quizData[currentQuestion].correctAnswer}</span>
 
-  const answerInput = 
+  const answerInput =
     <input
-    className='answer-section'
+      className='answer-section'
       type='text'
       value={userAnswer}
       onChange={handleAnswerInput}
       placeholder='Enter your answer'
     />
-  
+
 
   // 添加键盘事件监听
   useEffect(() => {
@@ -170,8 +165,26 @@ const App = () => {
   const onSelect = (selectedKeys, { node }) => {
     if (node.title in poemDic) {
       console.log('Selected node title:', node.title);
+      console.log('Selected node title c:', node.fromCollect);
+      if (node.fromCollect) {
+        const selectedPoemSentences = collectedSentences.filter(item => item.title === node.title);
+        const formattedQuizData = selectedPoemSentences.map((sentence, index) => ({
+          id: index,
+          questionText: sentence.content.split(', ')[0],
+          correctAnswer: sentence.content.split(', ')[1]
+        }));
+        // Sort formattedQuizData to match the order in poemDic[node.title]
+        const poemOrder = poemDic[node.title].map(item => item.questionText + ', ' + item.correctAnswer);
+        formattedQuizData.sort((a, b) => {
+          const aIndex = poemOrder.indexOf(a.questionText + ', ' + a.correctAnswer);
+          const bIndex = poemOrder.indexOf(b.questionText + ', ' + b.correctAnswer);
+          return aIndex - bIndex;
+        });
+        setQuizData(formattedQuizData);
+      } else {
+        setQuizData(poemDic[node.title]);
+      }
       setCurrentTitle(node.title);
-      setQuizData(poemDic[node.title]);
       setCurrentQuestion(0); // Reset to the first question
       setUserAnswer(''); // Clear user answer
       setFeedback(''); // Clear feedback
@@ -209,27 +222,27 @@ const App = () => {
       title: currentTitle,
       content: `${quizData[currentQuestion].questionText}, ${quizData[currentQuestion].correctAnswer}`
     };
-    
+
     const updatedCollectedSentences = [...collectedSentences];
-    
+
     const index = updatedCollectedSentences.findIndex(
       item => item.title === currentSentence.title && item.content === currentSentence.content
     );
-    
+
     if (index > -1) {
       updatedCollectedSentences.splice(index, 1);
     } else {
       updatedCollectedSentences.push(currentSentence);
     }
-    
+
     setCollectedSentences(updatedCollectedSentences);
     localStorage.setItem('collectedSentences', JSON.stringify(updatedCollectedSentences));
   };
 
   const isSentenceCollected = () => {
     return collectedSentences.some(
-      item => item.title === currentTitle && 
-      item.content === `${quizData[currentQuestion].questionText}, ${quizData[currentQuestion].correctAnswer}`
+      item => item.title === currentTitle &&
+        item.content === `${quizData[currentQuestion].questionText}, ${quizData[currentQuestion].correctAnswer}`
     );
   };
 
@@ -237,20 +250,20 @@ const App = () => {
     <div className='app'>
       <Layout style={{ minHeight: '100vh' }}>
         <Sider style={{ background: '#fff' }}>
-          <Tree 
-          defaultExpandAll = {true}
-          defaultSelectedKeys = {[9]}
-          treeData={combinedTreeData} onSelect={onSelect} />
+          <Tree
+            defaultExpandAll={true}
+            defaultSelectedKeys={[9]}
+            treeData={combinedTreeData} onSelect={onSelect} />
         </Sider>
         <Content style={{ padding: '20px' }}>
           <h3>
             {currentTitle}
           </h3>
           <>
-          <h3>
-            {currentQuestion + 1}  / { quizData.length}
-          </h3>
-            
+            <h3>
+              {currentQuestion + 1}  / {quizData.length}
+            </h3>
+
             <Button onClick={toggleRandom} style={{ margin: '0 10px' }}>
               {!isRandom ? '只考下半句' : '随机上下句'}
             </Button>
@@ -262,38 +275,39 @@ const App = () => {
             </Tooltip>
             <Tooltip title="Shortcut: =">
               <Button onClick={toggleShowAnswer} style={{ margin: '0 10px' }}>
-                {showCorrectAnswer ? 'Hide Answer' : 'Show Answer'}
+                {showCorrectAnswer ? '隐藏答案' : '查看答案'}
               </Button>
             </Tooltip>
-            <Tooltip title={isSentenceCollected() ? "Remove sentence from collection" : "Add sentence to collection"}>
-              <Button 
-                icon={isSentenceCollected() ? <HeartFilled /> : <HeartOutlined />} 
-                onClick={toggleCollectSentence} 
-                style={{ marginLeft: '10px' }}
-              />
-            </Tooltip>
+            
             <hr></hr>
             <div className='question-answer-container'>
               {answerUp === 1 ? (
                 <>
                   {questionSpan}
-                  <span style={{fontSize: '20px', margin : '8px'}}>,</span>
+                  <span style={{ fontSize: '20px', margin: '8px' }}>,</span>
                   {answerInput}
                 </>
               ) : (
                 <>
                   {answerInput}
-                  <span style={{fontSize: '20px', margin : '8px'}}>,</span>
+                  <span style={{ fontSize: '20px', margin: '8px' }}>,</span>
                   {questionSpan2}
                 </>
               )}
+              <Tooltip title={isSentenceCollected() ? "Remove sentence from collection" : "Add sentence to collection"}>
+              <Button
+                icon={isSentenceCollected() ? <HeartFilled /> : <HeartOutlined />}
+                onClick={toggleCollectSentence}
+                style={{ marginLeft: '10px' }}
+              />
+            </Tooltip>
             </div>
-              <div className='feedback'>{feedback}</div>
-              {showCorrectAnswer && (
-                <div className='correct-answer'>
-                  Correct Answer: {quizData[currentQuestion].questionText},  {quizData[currentQuestion].correctAnswer}
-                </div>
-              )}
+            <div className='feedback'>{feedback}</div>
+            {showCorrectAnswer && (
+              <div className='correct-answer'>
+                Correct Answer: {quizData[currentQuestion].questionText},  {quizData[currentQuestion].correctAnswer}
+              </div>
+            )}
           </>
 
 
