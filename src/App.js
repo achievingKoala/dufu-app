d54389105c482719a802a41eaec3889100fabe78
lkw <zgbtlkw@gmail.com>
2024-09-17 04:02:36 +0000

    icon

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
 
   const collectedTreeData = useMemo(() => {
     const collected = {
-      title: '收藏',
+      title: '收藏夹',
       key: 'collected',
       children: []
     };
